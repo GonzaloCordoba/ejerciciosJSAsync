2ef172fb5a96aab06764ef08a65acf3585f6c0c4
Cordoba <gc250268@ncr.com>
2020-09-28 17:14:54 -0300

    Resolucion ejercicio 4

--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -2,4 +2,8 @@
 const obtenerPokemon = require("./library");
 
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
-obtenerPokemon();
+obtenerPokemon("pikachu").then(res =>{
+    console.log(res.forms[0].name);
+    console.log("Lista de Hablidades: ");
+    res.abilities.forEach( h => console.log(h.ability.name));
+});
