c9e600540bc675f9e3db152b6f7eb85904739f58
Cordoba <gc250268@ncr.com>
2020-09-28 18:43:10 -0300

    Resolucion ejercicio 6

--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -2,4 +2,19 @@
 const obtenerPersonaFake = require("./library");
 
 // Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
-obtenerPersonaFake().then(x => console.log(x));
+obtenerPersonaFake().then( res =>{
+    res.results.forEach(persona => {
+        console.log("Nombre y Apellido: "+persona.name.first+" "+persona.name.last);
+        console.log("Genero: " + persona.gender);
+        console.log("Pais: " + persona.location.country);
+        console.log("Estado: " + persona.location.state);
+        console.log("Ciudad: " + persona.location.city);
+        console.log("Calle: " + persona.location.street.name + " nro: " + persona.location.street.number);
+        console.log("Codigo postal: "+ persona.location.postcode);
+        console.log("Email: " + persona.email);
+        console.log("Nombre de usuario: " + persona.login.username);
+        console.log("Contraseña: " + persona.login.password);
+
+    });
+  
+});
