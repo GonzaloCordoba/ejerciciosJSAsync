0f4b6ced694287aa5b0533de09386161fbf01196
Cordoba <gc250268@ncr.com>
2020-09-28 16:59:55 -0300

    Resolucion ejercicio 3

--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -2,13 +2,17 @@
 const obtenerPokemon = require("./library");
 
 // Codigo funcion callback
-
+function callback(res){
+    console.log(res.forms[0].name);
+    console.log("Lista de Hablidades: ");
+    res.abilities.forEach( h => console.log(h.ability.name));
+}
 
 
 // Fin codigo
-
+//charmander,pikachu
 // Usar la funcion obtenerPokemon(pokemon,funcionCallback) en donde:
 // - nombrePokemon es un string con el nombre del pokemon
 // - funcionCallback es una función que recibe el objeto con los datos del pokemon extraido
 
-obtenerPokemon();
+obtenerPokemon("pikachu",callback);
